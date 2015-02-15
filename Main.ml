a6629fea15b47817c72738c0f800f4cc643db4be
Eivy <modern.times.rock.and.roll+git@gmail.com>
2015-02-15 22:20:03 +0900

    write magic number in hex

--- Main.ml
+++ Main.ml
@@ -4,11 +4,11 @@ let getFormat f =
 	let rec loop ch a =
 		try
 			match a with
-			| [|255;216|] -> "jpg"
-			| [|137;80;78;71;13;10;26;10|] -> "png"
-			| [|71;73;70;56;55;97|] -> "gif"
-			| [|71;73;70;56;57;97|] -> "gif"
-			| [|66;77|] -> "bmp"
+			| [|0xff;0xd8|] -> "jpg"
+			| [|0x89;0x50;0x4e;0x47;0x0d;0x0a;0x1a;0x0a|] -> "png"
+			| [|0x47;0x49;0x46;0x38;0x37;0x61|] -> "gif"
+            | [|0x47;0x49;0x46;0x38;0x39;0x61|] -> "gif"
+			| [|0x42;0x4d|] -> "bmp"
 			| a -> if Array.length a > 10 then "unknown" else loop ch (Array.append a [|input_byte ic|])
 		with
 			End_of_file -> "unknown" in
